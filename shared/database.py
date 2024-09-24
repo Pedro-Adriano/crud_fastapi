c4d614d902df63d5f9204e1c9b45020d6a575884
Pedro-Adriano <pedrothiagoadriano@gmail.com>
2024-09-23 22:30:22 -0300

    feat: add dockerfile

--- shared/database.py
+++ shared/database.py
@@ -1,11 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:1234@localhost/db_fast_api_zero_ate_deploy"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("SQL_ALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
